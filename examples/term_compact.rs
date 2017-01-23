7e9eb4150e5f35f676b02b9cf13d9319853dcf78
Thayne McCombs <astrothayne@gmail.com>
2017-01-22 20:13:17 -0700

    Add slog_term factory

---
+++ examples/term_compact.rs
@@ -0,0 +1,33 @@
+#[macro_use]
+extern crate slog;
+extern crate slog_config;
+
+use std::{io, fs};
+use std::io::Read;
+use std::path::Path;
+
+use slog::{DrainExt, Logger};
+
+
+fn read_to_string(path: &str) -> io::Result<String> {
+    let mut file = fs::OpenOptions::new()
+        .read(true)
+        .open(Path::new(path))?;
+
+    let mut s = String::new();
+
+    let _ = file.read_to_string(&mut s)?;
+
+    Ok(s)
+}
+
+fn main() {
+    let config = read_to_string("examples/term-compact.toml").unwrap();
+
+    let drain = slog_config::from_config(&config).unwrap();
+    let logger = Logger::root(drain.fuse(), o!("test" => "term_compact"));
+
+    warn!(logger, "test warning");
+
+    info!(logger, "test complete");
+}
